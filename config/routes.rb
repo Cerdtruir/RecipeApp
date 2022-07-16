8818d783b6e7074d654cb8365265d08fa295abc5
Cerdtruir <ghoornabeel@gmail.com>
2022-07-16 17:04:56 +0200

    Set only necessary routes and add public recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :recipes
-  resources :foods
+  resources :users, only: %i[index show]
+
+  resources :recipes, only: %i[index show new create destroy]
+  resources :public_recipes, only: %i[index show]
+
+  resources :foods, only: %i[index new create destroy]
+
   devise_for :users, controllers: { registrations: 'users/registrations' }, path: ''
   root 'users#index'
-
-  resources :users, only: %i[index show]
 end
