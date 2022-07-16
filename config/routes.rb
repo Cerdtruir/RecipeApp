ed01f99cc9bb6cc98e73af0eb39395a8c312de80
Cerdtruir <ghoornabeel@gmail.com>
2022-07-16 12:44:53 +0200

    Add name to registration form controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :foods
-  devise_for :users, path: ''
+  devise_for :users, controllers: { registrations: 'users/registrations' }, path: ''
   root 'users#index'
 
   resources :users, only: %i[index show]
