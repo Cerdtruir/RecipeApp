b8df14381f4b05c9c04685cc783fce995da633f0
Cerdtruir <ghoornabeel@gmail.com>
2022-07-19 10:36:03 +0200

    Add recipe_food routes as sub routes under recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index show]
 
-  resources :recipes, only: %i[index show new create destroy]
+  resources :recipes, only: %i[index show new create destroy] do
+    resources :recipe_foods, only: %i[new create edit update destroy]
+  end
   resources :public_recipes, only: %i[index show]
 
   resources :foods, only: %i[index new create destroy]
