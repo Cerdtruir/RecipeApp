b903d68c043f1f516121d93b202dcdfb3125edfc
Cerdtruir <ghoornabeel@gmail.com>
2022-07-20 13:36:22 +0200

    Fix linter errors for new files

--- spec/views/shopping_lists/index.html.erb_spec.rb
+++ spec/views/shopping_lists/index.html.erb_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "shopping_lists/index", type: :view do
+RSpec.describe 'shopping_lists/index', type: :view do
   before(:each) do
     assign(:shopping_lists, [
-      ShoppingList.create!(),
-      ShoppingList.create!()
-    ])
+             ShoppingList.create!,
+             ShoppingList.create!
+           ])
   end
 
-  it "renders a list of shopping_lists" do
+  it 'renders a list of shopping_lists' do
     render
-    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
+    Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
   end
 end
